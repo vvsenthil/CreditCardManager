22c518acbc2aaeb4f8b6116fcd5988e52a700c61
vvsenthil <vvsenthil@gmail.com>
2022-07-30 23:20:51 +0100

    removed unwanted sysouts

--- src/main/java/com/sapient/card/controller/CreditCardController.java
+++ src/main/java/com/sapient/card/controller/CreditCardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.sapient.card.model.CreditCardRequest;
 import com.sapient.card.model.CreditCardResponse;
 import com.sapient.card.service.CreditCardService;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 public class CreditCardController {
 
